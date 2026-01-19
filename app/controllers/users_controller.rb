2a792fa197f52b2e396724b4456ae9c73692e4dd
Harry Boyes <harry.boyes@gmail.com>
2026-01-19 18:20:06 +0000

    feat: Add users/:user_id page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,6 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @rentals = @user.rentals.includes(book: :author)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
+  has_many :rentals
   has_one_attached :profile_image
 end
